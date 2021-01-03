da395bcfd65c79d7f4dade83f005d70b6f4f978d
Nathan Lam <nathan.lam@zoho.com>
2021-01-03 23:37:55 +1100

    Update backend url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('NEXT_PUBLIC_BACKEND_API_URL', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9f533b84d710a0e0aea0933fa5895a5a'),
