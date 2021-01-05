41524149577f9dc0284c8b66a7b3ed217a84d993
Nathan Lam <nathan.lam@zoho.com>
2021-01-05 23:36:05 +1100

    [WIP] Add like button

--- frontend/src/pages/blog/[slug].tsx
+++ frontend/src/pages/blog/[slug].tsx
@@ -1,10 +1,12 @@
 import { GetServerSideProps, NextPage } from 'next';
 import ErrorPage from 'next/error';
-import React from 'react';
-import axios from 'axios';
-import Container from '@material-ui/core/Container';
+import React, { MouseEventHandler, useState } from 'react';
 
-// import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { IconButton, Typography } from '@material-ui/core';
+import ThumbUpRoundedIcon from '@material-ui/icons/ThumbUpRounded';
+import Snackbar from '@material-ui/core/Snackbar';
+import Axios from 'axios';
 import Header from '../../components/Header';
 import { BlogPostModel } from '../../models/BlogPost';
 import BlogPostComponent from '../../components/BlogPost';
@@ -16,17 +18,51 @@ type Error = {
 type PropsWithError = BlogPostModel | Error;
 
 const BlogPost: NextPage<PropsWithError> = (props) => {
+  const [state, setState] = useState({
+    likes: 0,
+    hasLiked: false,
+    openSnackbar: false,
+  });
+
   const errorProps = props as Error;
   if (errorProps.error) {
     return <ErrorPage statusCode={404} />;
   }
-
   const { title, content, coverPhoto } = props as BlogPostModel;
+
+  const likeHandler: MouseEventHandler = async () => {
+    if (state.hasLiked) {
+      setState({ ...state, likes: state.likes - 1, hasLiked: !state.hasLiked });
+    } else {
+      setState({ ...state, likes: state.likes + 1, hasLiked: !state.hasLiked });
+    }
+  };
+
+  const handleClose = () => {
+    setState({ ...state, openSnackbar: false });
+  };
+
   return (
     <>
       <Header />
       <Container>
         <BlogPostComponent title={title} content={content} coverPhoto={coverPhoto} />
+        <div>
+          <IconButton
+            color={state.hasLiked ? 'primary' : 'inherit'}
+            aria-label="Like"
+            onClick={likeHandler}
+          >
+            <ThumbUpRoundedIcon />
+          </IconButton>
+          <Typography variant="button">{state.likes}</Typography>
+          <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            open={state.openSnackbar}
+            onClose={handleClose}
+            message="You need to sign in first"
+          />
+        </div>
       </Container>
     </>
   );
@@ -34,7 +70,7 @@ const BlogPost: NextPage<PropsWithError> = (props) => {
 
 export const getServerSideProps: GetServerSideProps<PropsWithError> = async ({ params }) => {
   try {
-    const { data } = await axios.get<BlogPostModel[]>(
+    const { data } = await Axios.get<BlogPostModel[]>(
       `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/blogs?slug=${params.slug}`
     );
 
